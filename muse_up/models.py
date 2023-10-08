7be0b837cafaa0903314d71c866cf5cd3c37a427
gary-ka <gantz1428@hotmail.com>
2023-10-08 18:58:10 +1000

    created User table

--- muse_up/models.py
+++ muse_up/models.py
@@ -1,2 +1,17 @@
 from flask_login import UserMixin
 from . import db
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    email_id = db.Column(db.String(100), index=True, nullable=False)
+    phone_num = db.Column(db.String(10), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    
+    #TODO: relationship with Event table and Booking table
+    comments = db.relationship('Comment', backref='user')
+    
+    def __repr__(self):
+        return f"Name: {self.name}"
